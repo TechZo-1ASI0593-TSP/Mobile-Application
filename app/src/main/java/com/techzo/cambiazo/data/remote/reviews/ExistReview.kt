c9e2d88ec6b91baa9b6844a5e55639261f03b74a
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-09-26 23:07:35 -0500

    chore: initial commit

--- app/src/main/java/com/techzo/cambiazo/data/remote/reviews/ExistReview.kt
+++
@@ -1,8 +0,0 @@
-package com.techzo.cambiazo.data.remote.reviews
-
-import com.google.gson.annotations.SerializedName
-
-data class ExistReview(
-    @SerializedName("existReview")
-    val existReview: Boolean
-)
