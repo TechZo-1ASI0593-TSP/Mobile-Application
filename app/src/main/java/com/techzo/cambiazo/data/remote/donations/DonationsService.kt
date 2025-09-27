0f951eb97af5bbf6b4366673546a06cfc8e8e5c0
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-09-26 23:07:35 -0500

    chore: initial commit

--- app/src/main/java/com/techzo/cambiazo/data/remote/donations/DonationsService.kt
+++
@@ -1,8 +0,0 @@
-package com.techzo.cambiazo.data.remote.donations
-
-import retrofit2.http.GET
-
-interface DonationsService {
-    @GET("donations/ongs")
-    suspend fun getAllOngs(): List<OngDto>
-}
