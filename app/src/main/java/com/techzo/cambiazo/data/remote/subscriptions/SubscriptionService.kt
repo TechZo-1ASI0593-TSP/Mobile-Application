482faa8c38dd015a227b3c2e4b557ede0ba4b68f
HelloNerk <josephhuamanilol@gmail.com>
2025-11-13 15:20:00 -0300

    feat:  added subscription dto and service

--- app/src/main/java/com/techzo/cambiazo/data/remote/subscriptions/SubscriptionService.kt
+++ app/src/main/java/com/techzo/cambiazo/data/remote/subscriptions/SubscriptionService.kt
@@ -1,6 +1,5 @@
 package com.techzo.cambiazo.data.remote.subscriptions
 
-import com.techzo.cambiazo.data.remote.products.ProductDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -9,28 +8,51 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
+/**
+ * Servicio API para gestionar suscripciones y planes.
+ */
 interface SubscriptionService {
 
+    // ------------------------------------------------------
     // SUBSCRIPTIONS
+    // ------------------------------------------------------
+
+    /**
+     * Obtiene la suscripción activa de un usuario específico.
+     */
     @Headers("Content-Type: application/json", "Accept: application/json")
     @GET("subscriptions/user/{id}")
-    suspend fun getUserSubscription(@Path("id") id: Int): Response<SubscriptionDto>
+    suspend fun getUserSubscription(
+        @Path("id") userId: Int
+    ): Response<SubscriptionDto>
 
-
+    /**
+     * Crea una nueva suscripción para un usuario.
+     */
     @Headers("Content-Type: application/json", "Accept: application/json")
     @POST("subscriptions")
-    suspend fun createSubscription(@Body subscriptionDto: SubscriptionRequestDto): Response<SubscriptionResponseDto>
+    suspend fun createSubscription(
+        @Body request: SubscriptionRequestDto
+    ): Response<SubscriptionResponseDto>
 
+    /**
+     * Actualiza el estado de una suscripción existente.
+     */
+    @Headers("Content-Type: application/json", "Accept: application/json")
     @PUT("subscriptions/status/{subscriptionId}")
     suspend fun updateSubscription(
         @Path("subscriptionId") subscriptionId: Int,
         @Body subscriptionRequest: SubscriptionRequestDto
     ): Response<SubscriptionResponseDto>
 
-    //PLANS
+    // ------------------------------------------------------
+    // PLANS
+    // ------------------------------------------------------
+
+    /**
+     * Obtiene la lista de todos los planes disponibles.
+     */
     @Headers("Content-Type: application/json", "Accept: application/json")
     @GET("plans")
     suspend fun getPlans(): Response<List<PlansDto>>
-
-
 }
